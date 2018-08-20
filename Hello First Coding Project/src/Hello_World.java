41b3f1a3b4607433b60964868732a61dd6366343
APCS3rd <APCS3rd@MHS-Math2-30>
2018-08-20 10:44:52 -0700

    First commit to github

---
+++ Hello First Coding Project/src/Hello_World.java
@@ -0,0 +1,13 @@
+/**
+ * Prints message in console
+ * 
+ * @author Vivian Lin
+ * @version 08/17/2018
+ */
+public class Hello_World
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Vivian"); //Display the string
+    }
+}
